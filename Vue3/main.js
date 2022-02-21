191e5cd91fb1ee5aa34cf3bb290b8fe2cd8e1bb7
AutumnSky-K <autumnsky1562@gmail.com>
2022-02-21 23:39:35 +0900

    vue.js 3 입문

---
+++ Vue3/main.js
@@ -0,0 +1,9 @@
+// {}에 들어가는 것을 Options object라고 부른다.
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'Sturdy',
+        }
+    }
+})
