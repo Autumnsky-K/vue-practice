4bae71a0a60ac1c7fd6cf7d62582d22ec6a446a5
AutumnSky-K <autumnsky1562@gmail.com>
2022-02-26 22:58:40 +0900

    data를 computed에서 계산하여 표현하기

--- Vue3/main.js
+++ Vue3/main.js
@@ -4,12 +4,13 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            brand: 'Vue Mastery',
+            onSale: true,
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ],
         }
     },
@@ -17,11 +18,25 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage;
+        updateVariant(index) {
+            this.selectedVariant = index;
         },
         removeFromCart() {
             this.cart -= 1
         },
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity;
+        },
+        showWhenSale() {
+            return this.onSale ? this.brand + ' ' + this.product : '';
+        }
     }
 })
